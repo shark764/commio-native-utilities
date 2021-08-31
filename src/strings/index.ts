be8d75d67ce92732edf24caf8cc47a6f514b0f7c
Farid Hernandez <farid.hdz.64@gmail.com>
2021-08-31 15:45:46 -0600

    feat: Adding tolower case test

--- src/strings/index.ts
+++ src/strings/index.ts
@@ -5,3 +5,5 @@ export const humanize = (s: string): string =>
   s
     .replace(/([A-Z])/g, ' $1')
     .replace(/^./, (str: string) => str.toUpperCase());
+
+export const toLower = (s: string): string => s.toLowerCase();
