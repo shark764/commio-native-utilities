17112a47121a401963ee69f25f4c02be90deca66
Farid Hernandez <farid.hdz.64@gmail.com>
2021-08-31 09:44:40 -0600

    fix: Moving tsconfig.base to root folder

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,4 @@ export * from './eslint-config';
 export * from './prettier-config';
 export * from './strings';
 export * from './svg-config';
-export * from './ts-config';
 export * from './utilities';
--- src/ts-config/index.ts
+++
@@ -1,3 +0,0 @@
-import tsconfigBase = require('./tsconfig.base.json');
-
-export { tsconfigBase };
